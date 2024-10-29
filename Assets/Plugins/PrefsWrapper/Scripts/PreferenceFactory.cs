36d202da94cbab3d90be54ca522177307a374640
doyasu24 <doyasu24@gmail.com>
2024-10-29 23:20:46 +0900

    separate

--- Assets/Plugins/PrefsWrapper/Scripts/PreferenceFactory.cs
+++ Assets/Plugins/PrefsWrapper/Scripts/PreferenceFactory.cs
@@ -1,13 +1,10 @@
 ﻿using System;
-using PrefsWrapper.Encoders;
 using PrefsWrapper.Serializers;
 
 namespace PrefsWrapper
 {
     public static class PreferenceFactory
     {
-        #region Pref
-
         public static IPreference<string> CreateStringPref(string key, bool enableMemCachePref = true)
         {
             return CreatePref(new StringPrefSerializer(), key, enableMemCachePref);
@@ -90,95 +87,5 @@ namespace PrefsWrapper
                 return new MemCachedPreference<T>(key, serializer);
             return new Preference<T>(key, serializer);
         }
-
-        #endregion
-
-        #region CryptoPref
-
-        public static IPreference<string> CreateStringCryptoPref(string key, string password, string salt)
-        {
-            return CreateCryptoPref(key, password, salt, new StringEncoder());
-        }
-
-        public static IPreference<int> CreateIntCryptoPref(string key, string password, string salt)
-        {
-            return CreateCryptoPref(key, password, salt, new IntEncoder());
-        }
-
-        public static IPreference<float> CreateFloatCryptoPref(string key, string password, string salt)
-        {
-            return CreateCryptoPref(key, password, salt, new FloatEncoder());
-        }
-
-        public static IPreference<bool> CreateBoolCryptoPref(string key, string password, string salt)
-        {
-            return CreateCryptoPref(key, password, salt, new BoolEncoder());
-        }
-
-        public static IPreference<byte> CreateByteCryptoPref(string key, string password, string salt)
-        {
-            return CreateCryptoPref(key, password, salt, new ByteEncoder());
-        }
-
-        public static IPreference<sbyte> CreateSByteCryptoPref(string key, string password, string salt)
-        {
-            return CreateCryptoPref(key, password, salt, new SByteEncoder());
-        }
-
-        public static IPreference<char> CreateCharCryptoPref(string key, string password, string salt)
-        {
-            return CreateCryptoPref(key, password, salt, new CharEncoder());
-        }
-
-        public static IPreference<short> CreateShortCryptoPref(string key, string password, string salt)
-        {
-            return CreateCryptoPref(key, password, salt, new ShortEncoder());
-        }
-
-        public static IPreference<ushort> CreateUShortCryptoPref(string key, string password, string salt)
-        {
-            return CreateCryptoPref(key, password, salt, new UShortEncoder());
-        }
-
-        public static IPreference<byte[]> CreateBinaryCryptoPref(string key, string password, string salt)
-        {
-            return CreateCryptoPref(key, password, salt, new NopEncoder());
-        }
-
-        public static IPreference<DateTime> CreateDateTimeCryptoPref(string key, string password, string salt)
-        {
-            return CreateCryptoPref(key, password, salt, new DateTimeEncoder());
-        }
-
-        public static IPreference<TimeSpan> CreateTimeSpanCryptoPref(string key, string password, string salt)
-        {
-            return CreateCryptoPref(key, password, salt, new TimeSpanEncoder());
-        }
-
-        /// <summary>
-        /// Create a preference to serialize Enum as string
-        /// </summary>
-        public static IPreference<T> CreateEnumCryptoPref<T>(string key, string password, string salt) where T : struct
-        {
-            return CreateCryptoPref(key, password, salt, new EnumEncoder<T>());
-        }
-
-        /// <summary>
-        /// Create a AES encoding preference to serialize using UnityEngine.JsonUtility
-        /// </summary>
-        public static IPreference<T> CreateJsonCryptoPref<T>(string key, string password, string salt)
-        {
-            return CreateCryptoPref(key, password, salt, new JsonEncoder<T>());
-        }
-
-        private static IPreference<T> CreateCryptoPref<T>(string key, string password, string salt, IEncoder<T> encoder)
-        {
-            var aesEncoder = new AesEncoder(password, salt);
-            var combinedEncoder = new CombinedEncoder<T>(encoder, aesEncoder);
-            var encodeSerializer = new EncodeSerializer<T>(new BinaryPrefSerializer(), combinedEncoder);
-            return new KeyEncodePreference<T>(key, encodeSerializer, aesEncoder);
-        }
-
-        #endregion
     }
 }
